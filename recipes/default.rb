5f2cb8deaeaa6c0910a74a1d916470e69ad31a09
Trotter Cashion <cashion@gmail.com>
2012-03-16 09:14:12 -0700

    Use cloudfoundry_component definition

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,20 +17,8 @@
 # limitations under the License.
 #
 
-ruby_path   = File.join(rbenv_root, "versions", node[:cloudfoundry_common][:ruby_1_9_2_version], "bin")
-config_file = File.join(node[:cloudfoundry_common][:config_dir], "dea.yml")
-dea_path    = File.join(node[:cloudfoundry_common][:vcap][:install_path], "bin", "dea")
-
 %w{lsof psmisc librmagick-ruby}.each do |pkg|
   package pkg
-end
-
-bash "install cloudfoundry-dea gems" do
-  user node[:cloudfoundry_common][:user]
-  cwd  File.join(node[:cloudfoundry_common][:vcap][:install_path], "dea")
-  code "#{File.join(ruby_path, "bundle")} install --without=test --local"
-  subscribes :run, resources(:git => node[:cloudfoundry_common][:vcap][:install_path])
-  action :nothing
 end
 
 directory node[:cloudfoundry_dea][:base_dir] do
@@ -43,24 +31,4 @@ node[:cloudfoundry_dea][:runtimes].each do |runtime|
   include_recipe runtime[:cookbook]
 end
 
-template config_file do
-  source "config.yml.erb"
-  owner  node[:cloudfoundry_common][:user]
-  mode   "0644"
-  notifies :restart, "bluepill_service[cloudfoundry-dea]"
-end
-
-template File.join(node[:bluepill][:conf_dir], "cloudfoundry-dea.pill") do
-  source "cloudfoundry-dea.pill.erb"
-  variables(
-    :path        => ruby_path,
-    :binary      => "#{File.join(ruby_path, "ruby")} #{dea_path}",
-    :pid_file    => node[:cloudfoundry_dea][:pid_file],
-    :config_file => config_file
-  )
-  notifies :restart, "bluepill_service[cloudfoundry-dea]"
-end
-
-bluepill_service "cloudfoundry-dea" do
-  action [:enable, :load, :start]
-end
+cloudfoundry_component "dea"
